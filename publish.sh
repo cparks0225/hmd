d8317510ea9f99f5fd4a28c51708e4875a9f9fc1
Chris Parks <cparks0225@gmail.com>
2014-12-14 02:15:23 -0600

    stable breakpoint in attempting to fix issues with responsiveness

--- publish.sh
+++ publish.sh
@@ -1,1 +1,4 @@
-scp -r . u77921023@s536449591.onlinehome.us:/kunden/homepages/38/d536449578/htdocs
+scp -r dist u77921023@s536449591.onlinehome.us:/kunden/homepages/38/d536449578/htdocs
+scp index.html u77921023@s536449591.onlinehome.us:/kunden/homepages/38/d536449578/htdocs
+scp html_form_send.php u77921023@s536449591.onlinehome.us:/kunden/homepages/38/d536449578/htdocs
+
