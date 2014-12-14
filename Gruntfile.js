fbee84fd04eca29a29b6d445e693390ae3cc7787
Chris Parks <cparks0225@gmail.com>
2014-12-14 03:52:11 -0600

    Cleaned up images, shrank background for quicker site loading

--- Gruntfile.js
+++ Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function(grunt) {
                 options: {
                     spawn: false,
                 }
-            }
+            },
             css: {
                 files: ['static/styles/*.scss'],
                 tasks: ['sass', 'clean', 'concat_css'],
